8162153a8149c0619a43feafac927b75d13d48e4
debashisnath <debashisnath1992@gmail.com>
2017-07-31 18:08:42 +0530

    general information editing state and business dropdown done

--- edit_general_information.php
+++ edit_general_information.php
@@ -15,7 +15,6 @@ if(isset($_POST['update_details']))
 	$business_type  = mysqli_real_escape_string($mysqli,$_POST['business_type']);
 	$address		= mysqli_real_escape_string($mysqli,$_POST['address']);
 	$state			= mysqli_real_escape_string($mysqli,$_POST['state']);
-	$country		= mysqli_real_escape_string($mysqli,$_POST['country']);
 	if($_FILES['image']['name'] == '')
 	{
 		$select_prev = mysqli_query($mysqli,"select * from tbl_business where business_id='$business_id'");
@@ -31,7 +30,7 @@ if(isset($_POST['update_details']))
 	move_uploaded_file($tmp_image,"uploads/".$savdse_image);
 	}
 
-	$update_customer_details = mysqli_query($mysqli, "update tbl_business set owners_firstname='".$firstname."', owners_lastname='".$lastname."', enterprise_name='".$company_name."', email='".$email."', work_phone='".$work_phone."', mobile='".$mobile."', 	gst_pan='".$gstin."', type_of_business='".$business_type."', address='".$address."', state='".$state."', country='".$country."', logo='".$savdse_image."'  where business_id='".$business_id."' ");
+	$update_customer_details = mysqli_query($mysqli, "update tbl_business set owners_firstname='".$firstname."', owners_lastname='".$lastname."', enterprise_name='".$company_name."', email='".$email."', work_phone='".$work_phone."', mobile='".$mobile."', 	gst_pan='".$gstin."', type_of_business='".$business_type."', address='".$address."', state='".$state."', country='INDIA', logo='".$savdse_image."'  where business_id='".$business_id."' ");
 	if($update_customer_details)
 	{
 		$data = "success";
@@ -84,7 +83,7 @@ if(isset($_POST['update_details']))
 									<div style="float:right;">
 										<!--<span style="padding:10px 10px;font-size:15px;font-weight:normal;color:#4a89dc;cursor:pointer;border-right:1px solid #CCC;"> <i class="fa fa-lightbulb-o"></i> &nbsp;&nbsp;Page Tutorial</span>-->
 
-										<span style="padding:10px 5px;font-size:25px;font-weight:normal;color:#000;cursor:pointer;" style="float:-right;" onclick="window.location.href='customer.php'"> <i class="fa fa-remove"></i> </span>
+										<span style="padding:10px 5px;font-size:25px;font-weight:normal;color:#000;cursor:pointer;" style="float:-right;" onclick="window.location.href='general_information.php'"> <i class="fa fa-remove"></i> </span>
 									</div>
 								</h3>
 								
@@ -225,16 +224,48 @@ if(isset($_POST['update_details']))
 												</div><!-- /.col-sm-4 -->
 											<div class="col-sm-8">
 											<div class="form-group">
-												<select class="form-control selectpicker" name="business_type">
-												<?php
-												$select_business_type = mysqli_query($mysqli,"select * from business_type");
-												while($fetch_type = mysqli_fetch_array($select_business_type)){
-												?>
-													 <option data-tokens="<?php echo $fetch_type['type_id'];?>" <?php echo(($fetch_type['type_id']==$fetch_query['type_of_business'])?'selected':'');?>><?php echo $fetch_type['name'];?></option>
-												<?php
-												}
-												?>
-												 </select>
+												<select name="business_type" class="form-control" >
+												
+												<option value= "<?php echo $fetch_query['type_of_business'];?>"><?php echo $fetch_query['type_of_business'];?></option>
+												
+													<option value="Accounting Services"> Accounting Services </option>
+													<option value="Administrative Services "> Administrative Services </option>
+													<option value="Advertising, Creative Design, Media and Marketing Services"> Advertising, Creative Design, Media and Marketing Services </option>
+													<option value="Agriculture, Farming, Fishing and Forestry"> Agriculture, Farming, Fishing and Forestry </option>
+													<option value="Amusement, Gambling, and Recreation"> Amusement, Gambling, and Recreation </option>
+													<option value="Animal Services"> Animal Services </option>
+													<option value="Architectural, Engineering, Design and Related Services"> Architectural, Engineering, Design and Related Services </option>
+													<option value="CA/TAX Consultant"> CA/TAX Consultant </option>
+													<option value="Care Givers"> Care Givers </option>
+													<option value="Charity, Nonprofits and Similar Groups"> Charity, Nonprofits and Similar Groups </option>
+													<option value="Church"> Church </option>
+													<option value="Computer Systems Design and Related Services"> Computer Systems Design and Related Services </option>
+													<option value="Construction"> Construction </option>
+													<option value="Consulting, Professional and Technical Services"> Consulting, Professional and Technical Services </option>
+													<option value="Educational Services"> Educational Services </option>
+													<option value="Finance and Insurance"> Finance and Insurance </option>
+													<option value="Food & Beverage Establishments"> Food & Beverage Establishments </option>
+													<option value="Freelancer"> Freelancer </option>
+													<option value="General Service-Based Business"> General Service-Based Business </option>
+													<option value="Hair Salons, Barbers and Spas"> Hair Salons, Barbers and Spas </option>
+													<option value="Healthcare Services"> Healthcare Services </option>
+													<option value="Human Resources and Placement Consulting"> Human Resources and Placement Consulting </option>
+													<option value="IT & Telecommunications"> IT & Telecommunications </option>
+													<option value="Land and Property including Management"> Land and Property including Management </option>
+													<option value="Landscaping and Gardening Services"> Landscaping and Gardening Services </option>
+													<option value="Learning Institutes"> Learning Institutes </option>
+													<option value="Legal Services"> Legal Services </option>
+													<option value="Manufacturers"> Manufacturers </option>
+													<option value="Manufacturing and Mining"> Manufacturing and Mining </option>
+													<option value="Performing Arts, Spectator Sports, and Related Industries"> Performing Arts, Spectator Sports, and Related Industries </option>
+													<option value="Repair and Maintenance Services"> Repair and Maintenance Services </option>
+													<option value="Retail Shops, Mail Order and Online"> Retail Shops, Mail Order and Online </option>
+													<option value="Transportation and Warehousing"> Transportation and Warehousing </option>
+													<option value="Travel and Tourism Services"> Travel and Tourism Services </option>
+													<option value="Vehicle Sales, Maintenance and Repairs"> Vehicle Sales, Maintenance and Repairs </option>
+													<option value="Whole-sellers"> Whole-sellers </option>
+													<option value="Wholesale Trade and Distributors"> Wholesale Trade and Distributors </option>
+												</select>
 											</div>
 											</div><!-- /.form-group -->
 											</div>
@@ -244,7 +275,7 @@ if(isset($_POST['update_details']))
 													<div class="form-group">
 														<label><b>Address : </b></label>
 													</div><!-- /.form-group -->
-												</div><!-- /.col-sm-4 -->
+											</div><!-- /.col-sm-4 -->
 											<div class="col-sm-8">
 											<div class="form-group">
 												<input name="address" type="text" class="form-control" id="rs-form-example-fname" value=<?php echo $fetch_query['address']; ?>>
@@ -258,30 +289,28 @@ if(isset($_POST['update_details']))
 													<div class="form-group">
 														<label><b>State : </b></label>
 													</div><!-- /.form-group -->
-												</div><!-- /.col-sm-4 -->
+											</div><!-- /.col-sm-4 -->
 											<div class="col-sm-8">
 											<div class="form-group">
-												<input name="state" type="text" class="form-control" id="rs-form-example-fname" value=<?php echo $fetch_query['state']; ?>>
+												<select name="state" class="form-control billstate1" required>
+													
+												<?php
+												$state_info = mysqli_query($mysqli, "select * from states");
+												while ($fetch_bill_state = mysqli_fetch_array($state_info))
+												{
+												?>
+												<option value= "<?php echo $fetch_bill_state['states_code'];?>" <?php echo(($fetch_query['state']==$fetch_bill_state['states_code'])?'selected':'');
+												?>><?php echo $fetch_bill_state['states_name'];?>&nbsp;(<?php echo $fetch_bill_state['states_code'];?>)
+												</option>
+												<?php
+												}
+												?>
+												</select>
 												<p class="help-block with-errors"></p>
-											</div>
 											</div><!-- /.form-group -->
-											</div>
-
-											<div class="row">
-												<div class="col-sm-3">
-														<div class="form-group">
-															<label><b>Country : </b></label>
-														</div><!-- /.form-group -->
-													</div><!-- /.col-sm-4 -->
-												<div class="col-sm-8">
-												<div class="form-group">
-													<input name="country" type="text" class="form-control" id="rs-form-example-fname" value=<?php echo $fetch_query['country']; ?>>
-													<p class="help-block with-errors"></p>
-												</div>
-												</div><!-- /.form-group -->
-											</div>
-
-								</div><!-- /.panel-body -->
+										</div>
+										</div>
+									</div><!-- /.panel-body -->
 							</div><!-- /.panel -->
 						</div>
 

437c946cf0f0e284a258abd1366f89bd2f092306
Deep Bratt Mondal <deepbratt@gmail.com>
2017-08-04 04:31:20 +0530

    adsda
    dasada

--- frontend/footer.php
+++ frontend/footer.php
@@ -1,31 +1,4 @@
         <footer id="footer" class="light-footer">
-            <div class="sub-footer ptb-20">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-8 col-sm-8 col-xs-12">
-                            <form class="form-inline form-subscribe">
-                                <div class="input-group">
-                                    <input type="email" class="form-control" placeholder="Enter Email">
-                                    <span class="input-group-btn">
-                                        <button class="btn btn-suscribe" type="button">SUBSCRIBE</button>
-                                    </span>
-                                </div><!-- /input-group -->
-
-                            </form>
-                        </div>
-				
-                        <div class="col-md-4 col-sm-4 col-xs-12">
-                            <ul class="footer-social hi-icon-effect-8">
-                                <li><a href="#" class="hi-icon glyphicon-facebook"></a></li>
-                                <li><a href="#" class="hi-icon glyphicon-twitter"></a></li>
-                                <li><a href="#" class="hi-icon glyphicon-linkedin"></a></li>
-                                <li><a href="#" class="hi-icon glyphicon-google"></a></li>
-                            </ul>
-							
-                        </div>
-                    </div><!-- End Container -->
-                </div><!-- End Sub Footer -->
-            </div>
             <div class="footer-bottom ptb-20 paraxify">
                 <div class="container">
                     <div class="row">
@@ -33,7 +6,7 @@
                             &COPY; Port me. All Rights are Reserved.
                         </div>
                         <div class="col-md-4 col-sm-4 col-xs-12 footer-logo text-center">
-                            <img src="images/LOGO/banner_logo.png" alt="logo" style="">
+                           &nbsp;
                         </div>
                         <div class="col-md-4 col-sm-4 col-xs-12 back-to-top">
                             <a href="#top" id="back-top"><span class="ti-arrow-circle-up"></span></a>
